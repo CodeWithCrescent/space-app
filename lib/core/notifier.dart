a4eef172ca690c4d75acc5190f3ab67c673564b4
Crescent Geniius <crescent.sambila@gmail.com>
2024-01-29 03:55:34 +0300

    Adding the comments in notifier

--- lib/core/notifier.dart
+++ lib/core/notifier.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// This is for creating the dark and light mode toggle button.
+///   ..and here is, at first the Value Notifier for dark mode is false, means the app is
+///   at light mode.
+
 ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(false);
